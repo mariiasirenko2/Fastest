1dcb4a4b7764e870a68b1da8f49634a82c1096bd
Mariia <mariiasirenko2@gmail.com>
2022-04-21 14:48:41 +0300

    Splash Screen and Login added

--- app/src/main/java/com/fastastapp/MainActivity.java
+++ app/src/main/java/com/fastastapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.fastastapp;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN = 3000;
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.imageView);
+        slogan = findViewById(R.id.textView2);
+
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(() -> {
+
+            Intent intent=new Intent(MainActivity.this,Login.class);
+
+            ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,
+                    new Pair<>(image, "logo_image"));
+            startActivity(intent,options.toBundle());
+        },SPLASH_SCREEN);
+
+
+
+
     }
 }
